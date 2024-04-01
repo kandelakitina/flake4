07a8338bdeea38ca6c187a5d5a999cc4d56c8eef
boticelli <kandelakitina@gmail.com>
2024-04-01 11:39:50 +0300

    Add impermanence to flake and modules

--- flake.nix
+++ flake.nix
@@ -29,8 +29,12 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
 
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = inputs:
     inputs.snowfall-lib.mkFlake {
